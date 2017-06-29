3023e6e5225a88cad4c28882e23ece8a23f161f9
Derik Evangelista <devangelista@pivotal.io>
2017-06-29 15:13:14 +0100

    Add task to backup director
    [#147502235]
    
    Signed-off-by: Ed Cook <ecook@pivotal.io>

---
+++ tasks/bbr-backup-director/task.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eu
+
+. "$(dirname $0)"/../../scripts/export-director-metadata
+
+om_cmd curl -p /api/v0/deployed/director/credentials/bbr_ssh_credentials > bbr_keys.json
+BOSH_PRIVATE_KEY=$(jq -r '.credential.value.private_key_pem' bbr_keys.json)
+
+pushd director-backup-artifact
+  ../binary/bbr director --host "${BOSH_ADDRESS}" \
+  --username bbr \
+  --private-key-path <(echo "${BBR_PRIVATE_KEY}") \
+  backup
+
+  tar -cvf director-backup.tar -- *
+popd
