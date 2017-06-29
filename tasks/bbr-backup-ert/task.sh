f2cb5c51186a2f87faef95d1fe82e5aeffced9fb
Ed Cook <ecook@pivotal.io>
2017-06-29 14:26:29 +0100

    Run bbr-backup-ert from a tagged worker
    to keep it consistent with how cfops is currently deployed
    
    [#145837859]
    
    Signed-off-by: Derik Evangelista <devangelista@pivotal.io>

--- tasks/bbr-backup-ert/task.sh
+++ tasks/bbr-backup-ert/task.sh
@@ -1,12 +1,6 @@
 #!/bin/bash -eu
 
-echo "${JUMPBOX_SSH_KEY}" > jumpbox.pem
-chmod 600 jumpbox.pem
-eval "$(ssh-agent)" && ssh-add jumpbox.pem
-
 . "$(dirname $0)"/../../scripts/export-director-metadata
-
-sshuttle -r "${JUMPBOX_USER}@${JUMPBOX_HOST}" 0/0 --daemon
 
 pushd ert-backup-artifact
   ../binary/bbr deployment --target "${BOSH_ADDRESS}" \
