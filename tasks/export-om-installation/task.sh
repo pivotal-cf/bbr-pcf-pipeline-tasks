3925d8f121c25b188c3962e04bf3592f8451ec2d
Henry Stanley <hstanley@pivotal.io>
2017-09-05 11:03:10 +0100

    Increase timeout on OpsMan export installation
    [#150742245]
    
    Signed-off-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

--- tasks/export-om-installation/task.sh
+++ tasks/export-om-installation/task.sh
@@ -2,4 +2,4 @@
 
 . $(dirname $0)/../../scripts/om-cmd
 
-om_cmd export-installation --output-file om-installation/installation.zip
+om_cmd --request-timeout 7200 export-installation --output-file om-installation/installation.zip
