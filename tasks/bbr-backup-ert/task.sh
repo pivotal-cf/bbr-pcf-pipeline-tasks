63a5778a0715d47789d31f1fc14ddbe7aedb6bda
Mirah Gary <mgary@pivotal.io>
2018-09-13 13:40:45 +0100

    Support running bbr-backup-ert using BOSH_ALL_PROXY
    If OPSMAN_PRIVATE_KEY param is provided, the task will export
    BOSH_ALL_PROXY, which is used by bbr v1.2.6+ to create an ssh tunnel via
    the Ops Manager VM.
    
    [#160403281]
    
    Signed-off-by: Josh Hill <jhill@pivotal.io>

--- tasks/bbr-backup-ert/task.sh
+++ tasks/bbr-backup-ert/task.sh
@@ -1,13 +1,25 @@
-#!/bin/bash -eu
+#!/bin/bash
 
-. "$(dirname $0)"/../../scripts/export-director-metadata
+set -eu
+
+# shellcheck disable=SC1090
+source "$(dirname "$0")"/../../scripts/export-director-metadata
+
+if [ ! -z "$OPSMAN_PRIVATE_KEY" ]; then
+  echo -e "$OPSMAN_PRIVATE_KEY" > "${PWD}/ssh.key"
+  chmod 0600 "${PWD}/ssh.key"
+  opsman_private_key_path="${PWD}/ssh.key"
+  opsman_host="$(basename "$OPSMAN_URL")"
+  export BOSH_ALL_PROXY="ssh+socks5://ubuntu@${opsman_host}:22?private-key=${opsman_private_key_path}"
+  echo "Using BOSH_ALL_PROXY"
+fi
 
 pushd ert-backup-artifact
-  ../binary/bbr deployment --target "${BOSH_ADDRESS}" \
-  --username "${BOSH_CLIENT}" \
-  --deployment "${ERT_DEPLOYMENT_NAME}" \
-  --ca-cert "${BOSH_CA_CERT_PATH}" \
-  backup --with-manifest
+  ../binary/bbr deployment --target "$BOSH_ADDRESS" \
+    --username "$BOSH_CLIENT" \
+    --deployment "$ERT_DEPLOYMENT_NAME" \
+    --ca-cert "$BOSH_CA_CERT_PATH" \
+    backup --with-manifest
 
   tar -cvf ert-backup.tar -- *
 popd
