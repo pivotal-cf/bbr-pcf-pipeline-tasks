9189916a38f473e2dd5c305044855ae81d473775
Josh Hill <jhill@pivotal.io>
2018-10-01 15:50:29 +0100

    Rename BOSH_ADDRESS -> BOSH_ENVIRONMENT
    [#160717638]
    
    Signed-off-by: Glen Rodgers <grodgers@pivotal.io>

--- tasks/bbr-cleanup-ert/task.sh
+++ tasks/bbr-cleanup-ert/task.sh
@@ -7,7 +7,7 @@ source "$(dirname "$0")/../../scripts/export-director-metadata"
 # shellcheck disable=SC1090
 source "$(dirname "$0")/../../scripts/export-cf-metadata"
 
-./binary/bbr deployment --target "$BOSH_ADDRESS" \
+./binary/bbr deployment --target "$BOSH_ENVIRONMENT" \
   --username "$BOSH_CLIENT" \
   --deployment "$CF_DEPLOYMENT_NAME" \
   --ca-cert "$BOSH_CA_CERT_PATH" \
