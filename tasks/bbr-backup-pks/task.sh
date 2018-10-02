4e1ab093ae036bbf43de3e0f2d82c94808e02a44
Jake Klein <jklein@pivotal.io>
2018-10-02 12:28:27 +0100

    Created pks backup and backup-cleanup tasks
    Pulled out backup/cleanup logic into scripts to be reused
    [#160717638]
    
    Signed-off-by: Glen Rodgers <grodgers@pivotal.io>

--- tasks/bbr-backup-pks/task.sh
+++ tasks/bbr-backup-pks/task.sh
@@ -5,10 +5,10 @@ set -eu
 # shellcheck disable=SC1090
 source "$(dirname "$0")/../../scripts/export-director-metadata"
 # shellcheck disable=SC1090
-source "$(dirname "$0")/../../scripts/export-cf-metadata"
+source "$(dirname "$0")/../../scripts/export-pks-metadata"
 
-pushd ert-backup-artifact
+pushd pks-backup-artifact
   # shellcheck disable=SC1090
   source "$(dirname "$0")/../../scripts/deployment-backup"
-  tar -cvf ert-backup.tar -- *
+  tar -cvf pks-backup.tar -- *
 popd
--- tasks/bbr-cleanup-pks/task.sh
+++ tasks/bbr-cleanup-pks/task.sh
@@ -5,7 +5,7 @@ set -eu
 # shellcheck disable=SC1090
 source "$(dirname "$0")/../../scripts/export-director-metadata"
 # shellcheck disable=SC1090
-source "$(dirname "$0")/../../scripts/export-cf-metadata"
+source "$(dirname "$0")/../../scripts/export-pks-metadata"
 
 # shellcheck disable=SC1090
 source "$(dirname "$0")/../../scripts/deployment-backup-cleanup"
