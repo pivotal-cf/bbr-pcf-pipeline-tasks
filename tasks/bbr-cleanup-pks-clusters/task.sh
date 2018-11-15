4d2a557dc82860e6d5eb4284404fc69254565acd
Chunyi Lyu <clyu@pivotal.io>
2018-11-15 15:45:20 +0000

    Add cleanup-pks-clusters task
    [#160717790]
    
    Signed-off-by: Jake Klein <jklein@pivotal.io>

---
+++ tasks/bbr-cleanup-pks-clusters/task.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eu
+
+scripts="$(dirname "$0")/../../scripts"
+
+
+# shellcheck disable=SC1090
+source "$scripts/export-director-metadata"
+# shellcheck disable=SC1090
+source "$scripts/export-pks-metadata"
+
+# shellcheck disable=SC1090
+source "$scripts/all-deployment-backup-cleanup"
