1435e937249c0f4c35e245d49eb1338c4b2a5441
patrickhuber <patrickhuber@users.noreply.github.com>
2019-01-09 12:31:34 -0500

    adds noproxy setting updates tar command to remove during archive

--- tasks/bbr-backup-director-to-azure/task.sh
+++ tasks/bbr-backup-director-to-azure/task.sh
@@ -42,7 +42,7 @@ function upload_to_azure(){
         --name "$AZURE_STORAGE_VERSIONED_FILE"        
 }
 
-# get script directory
+# get script and task root directories
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 ROOT=$DIR/../../..
 
@@ -52,8 +52,17 @@ chmod +x /usr/local/bin/om
 
 source $ROOT/bbr-pipeline-tasks-repo/scripts/export-director-metadata
 
+# add director IP to no_proxy if requested. this must follow export-director-metadata
+[[ ",$no_proxy," != *",${BOSH_ENVIRONMENT},"* ]] \
+&& [[ "$ADD_DIRECTOR_TO_NO_PROXY" == "true" ]] \
+&& no_proxy="${BOSH_ENVIRONMENT},${no_proxy}"
+
+# subshell trick to implement a try/catch style command.
+# we always want to cleanup our mess so capture any error in the subshell.
+# this will capture the error code in the subshell and if it is non zero, return a failure after cleanup
 set +e
 (
+    # exit subshell on error
     set -e
     mkdir -p $ROOT/director-backup-artifact
     pushd $ROOT/director-backup-artifact
@@ -62,13 +71,14 @@ set +e
         backup_director   
 
         echo "compressing backup"
-        tar -cvzf director-backup.tgz -- *
+        tar cvzf director-backup.tgz -- * | xargs rm -f
 
     popd
 
     upload_to_azure
 )
 
+# capture subshell return code and go back to failing the script on error
 return_code=$?
 set -e
 
@@ -76,6 +86,8 @@ set -e
 echo "cleaning up backup"
 rm -rf $ROOT/director-backup-artifact/*
 
+# if return code from the subshell above was non zero
 if [ $return_code -ne 0 ]; then
+  # return the return code (this will fail the task)
   exit $return_code
 fi
--- tasks/bbr-backup-ert-to-azure/task.sh
+++ tasks/bbr-backup-ert-to-azure/task.sh
@@ -50,8 +50,17 @@ chmod +x /usr/local/bin/om
 source $ROOT/bbr-pipeline-tasks-repo/scripts/export-director-metadata
 source $ROOT/bbr-pipeline-tasks-repo/scripts/export-cf-metadata
 
+# set the no proxy to contain the director ip if requested. this must follow export-director-metadata
+[[ ",$no_proxy," != *",${BOSH_ENVIRONMENT},"* ]] \
+&& [[ "$ADD_DIRECTOR_TO_NO_PROXY" == "true" ]] \
+&& no_proxy="${BOSH_ENVIRONMENT},${no_proxy}"
+
+# subshell trick to implement a try/catch style command.
+# we always want to cleanup our mess so capture any error in the subshell.
+# this will capture the error code in the subshell and if it is non zero, return a failure after cleanup
 set +e
 (
+    # exit subshell on error
     set -e
     mkdir -p $ROOT/ert-backup-artifact
     pushd $ROOT/ert-backup-artifact
@@ -60,13 +69,13 @@ set +e
         backup_pas 
 
         echo "compressing backup"
-        tar -cvzf ert-backup.tgz -- *
-
+        tar cvzf ert-backup.tgz -- * | xargs rm -f
     popd
 
     upload_to_azure
 )
 
+# capture subshell return code and go back to failing the script on error
 return_code=$?
 set -e
 
@@ -74,6 +83,8 @@ set -e
 echo "cleaning up backup"
 rm -rf $ROOT/ert-backup-artifact/*
 
+# if return code from the subshell above was non zero
 if [ $return_code -ne 0 ]; then
+  # return the return code (this will fail the task)
   exit $return_code
 fi
