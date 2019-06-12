49df8f35737a6c128e7a1b65711115ab9f48ca1e
Jake Klein <jklein@pivotal.io>
2019-06-12 12:02:35 +0100

    Only message output to stdout is the apply changes message
    [#165442961]
    
    Signed-off-by: Glen Rodgers <grodgers@pivotal.io>

--- tasks/check-opsman-status/task.sh
+++ tasks/check-opsman-status/task.sh
@@ -5,13 +5,13 @@ set -euo pipefail
 scripts="$(dirname "$0")/../../scripts"
 
 # shellcheck source=../../scripts/om-cmd
-source "${scripts}/om-cmd"
+source "${scripts}/om-cmd" > /dev/null
 
 status="$(om_cmd installations --format json | jq .[0].status)"
 trimmed_status="$(xargs <<< "$status")"
 
 if [ "$trimmed_status" == "running" ]; then
-  echo "\"Apply Changes\" is in flight." 2>&1
+  echo "\"Apply Changes\" is in flight." | tee /dev/stder
   exit 1
 fi
 
